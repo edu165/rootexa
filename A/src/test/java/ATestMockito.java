549a1f7f98a70dc9304b81e7098dec84df9aaa46
Eduardo Herrera <eduardoherrera165@gmail.com>
2016-05-26 09:54:34 +0200

    Signed-off-by: Eduardo Herrera <eduardoherrera165@gmail.com>fff

--- A/src/test/java/ATestMockito.java
+++ A/src/test/java/ATestMockito.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.Reader;
+import java.net.Socket;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
@@ -10,6 +11,7 @@ import java.sql.SQLException;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,20 +22,42 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class ATestMockito {
 	A clase = new A();
+	@Mock
+	Connection con;
+	@Mock
+	DatabaseMetaData dbmd;
+	@Mock
+	ResultSet rs;
+	
+	
 
-@Mock
-private Connection databaseConnection;
+
+
 @Mock
  private DatabaseMetaData metadata;
 @Mock
-private ResultSet result;
+private ResultSet rmd;
+@Mock
+private SSLSocketFactory sslFactory;
+@Mock
+private HttpsURLConnection connection;
+@Mock
+private Socket createSocket;
+
 @Test
-public void isAutoIncrement() throws SQLException, IOException  {
-	Mockito.when(databaseConnection.getMetaData()).thenReturn(metadata);
-	Mockito.when(metadata.getIndexInfo("", "", "", true, false)).thenReturn(result);
-	Mockito.when(result.getMetaData().isAutoIncrement(0)).thenReturn(Boolean.TRUE);
-	
-	clase.isAutoIncrement(databaseConnection);
+public void TestisAutoIncrement() throws SQLException, IOException  {
+	Mockito.when(con.getMetaData()).thenReturn(dbmd);
+	Mockito.when(dbmd.getIndexInfo("", "", "", true, false)).thenReturn(rs);
+	Mockito.when(rs.getMetaData()).thenReturn(rmd);
+	clase.isAutoIncrement(con);
+}
+
+@Test
+public void  TestcreateSocket() throws SQLException, IOException  {
+	Mockito.when(connection.getSSLSocketFactory()).thenReturn(sslFactory);
+	Mockito.when(sslFactory.createSocket("localhost", 8080)).thenReturn(createSocket);
+
+	clase.createSocket(connection);
 }
 
 
